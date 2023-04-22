0042864fbb0e86c685e7873e9096b9c0bb801498
drewskis <drewskis@umich.edu>
2023-04-22 01:09:18 -0400

    Literally everything

---
+++ lib/datasets/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2020 NVIDIA Corporation. All rights reserved.
+# This work is licensed under the NVIDIA Source Code License - Non-commercial. Full
+# text can be found in LICENSE.md
+
+from .imdb import imdb
+from .tabletop_object import TableTopObject
+from .osd_object import OSDObject
+from .ocid_object import OCIDObject
+#from .build_data import BuildDataLoader
+
+import os.path as osp
+ROOT_DIR = osp.join(osp.dirname(__file__), '..', '..')
